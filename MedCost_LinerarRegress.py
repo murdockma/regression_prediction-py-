7f3d9d5c89962530f0a9d4c3e6cadd962eb5bb58
murdockma <47290536+murdockma@users.noreply.github.com>
2021-05-25 14:50:51 -0600

    Update MedCost_LinerarRegress.py

--- MedCost_LinerarRegress.py
+++ MedCost_LinerarRegress.py
@@ -60,64 +60,8 @@ sns.lmplot(x="bmi", y="charges", hue="smoker", data=data, palette = 'magma', siz
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.ensemble import RandomForestRegressor
 
-X = data.drop(['charges'], axis = 1)
-Y = data.charges
-
-
-
-quad = PolynomialFeatures (degree = 2)
-x_quad = quad.fit_transform(X)
-
-X_train,X_test,Y_train,Y_test = train_test_split(x_quad,Y, random_state = 0)
-
-plr = LinearRegression().fit(X_train,Y_train)
-
-Y_train_pred = plr.predict(X_train)
-Y_test_pred = plr.predict(X_test)
-
-print(plr.score(X_test,Y_test))
-
-
-# In[ ]:
-
-
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split_
-
-
-# In[96]:
-
-
-forest = RandomForestRegressor(n_estimators = 100,
-    criterion = 'mse',
-    random_state = 1,
-    n_jobs = -1)
-forest.fit(x_train,y_train)
-forest_train_pred = forest.predict(x_train)
-forest_test_pred = forest.predict(x_test)
-
-print('MSE train data: %.3f, MSE test data: %.3f' % (
-mean_squared_error(y_train,forest_train_pred),
-mean_squared_error(y_test,forest_test_pred)))
-print('R2 train data: %.3f, R2 test data: %.3f' % (
-r2_score(y_train,forest_train_pred),
-r2_score(y_test,forest_test_pred)))
-
-plt.figure(figsize=(10,6))
-
-plt.scatter(forest_train_pred,forest_train_pred - y_train,
-          c = 'black', marker = 'o', s = 35, alpha = 0.5,
-          label = 'Trained')
-plt.scatter(forest_test_pred,forest_test_pred - y_test,
-          c = 'c', marker = 'o', s = 35, alpha = 0.7,
-          label = 'Tested')
-plt.xlabel('Predicted values')
-plt.ylabel('Tailings')
-plt.legend(loc = 'upper left')
-plt.hlines(y = 0, xmin = 0, xmax = 60000, lw = 2, color = 'red')
-plt.show()
-
+### currently working on the model
